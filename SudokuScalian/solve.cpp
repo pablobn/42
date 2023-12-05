72b5dd44a8c84b9ae9a7f01765ed11e30ce5ca43
Pablo Bengoechea <93341501+pablobn@users.noreply.github.com>
2023-12-05 14:01:45 +0100

    Update solve.cpp

--- SudokuScalian/solve.cpp
+++ SudokuScalian/solve.cpp
@@ -1,80 +1,103 @@
 #include <iostream>
-#define N 9
-int grid[N][N] = {
-   {3, 0, 6, 5, 0, 8, 4, 0, 0},
-   {5, 2, 0, 0, 0, 0, 0, 0, 0},
-   {0, 8, 7, 0, 0, 0, 0, 3, 1},
-   {0, 0, 3, 0, 1, 0, 0, 8, 0},
-   {9, 0, 0, 8, 6, 3, 0, 0, 5},
-   {0, 5, 0, 0, 9, 0, 6, 0, 0},
-   {1, 3, 0, 0, 0, 0, 2, 5, 0},
-   {0, 0, 0, 0, 0, 0, 0, 7, 4},
-   {0, 0, 5, 2, 0, 6, 3, 0, 0}
+
+using namespace std;
+
+int sudoku[9][9] = {
+   {5, 3, 0, 0, 7, 0, 0, 0, 0},
+   {6, 0, 0, 1, 9, 5, 0, 0, 0},
+   {0, 9, 8, 0, 0, 0, 0, 6, 0},
+   {8, 0, 0, 0, 6, 0, 0, 0, 3},
+   {4, 0, 0, 8, 0, 3, 0, 0, 1},
+   {7, 0, 0, 0, 2, 0, 0, 0, 6},
+   {0, 6, 0, 0, 0, 0, 2, 8, 0},
+   {0, 0, 0, 4, 1, 9, 0, 0, 5},
+   {0, 0, 0, 0, 8, 0, 0, 7, 9}
 };
-bool isPresentInCol(int col, int num){ //check whether num is present in col or not
-   for (int row = 0; row < N; row++)
-      if (grid[row][col] == num)
-         return true;
-   return false;
+
+int isInCol(int col, int num)
+{
+   for (int row = 0; row < 9; row++)
+      if (sudoku[row][col] == num)
+         return (1);
+   return (0);
 }
-bool isPresentInRow(int row, int num){ //check whether num is present in row or not
-   for (int col = 0; col < N; col++)
-      if (grid[row][col] == num)
-         return true;
-   return false;
+
+int isInRow(int row, int num)
+{
+   for (int col = 0; col < 9; col++)
+      if (sudoku[row][col] == num)
+         return (1);
+   return (0);
 }
-bool isPresentInBox(int boxStartRow, int boxStartCol, int num){
-//check whether num is present in 3x3 box or not
+
+int isInBox(int startRow, int startCol, int num)
+{
    for (int row = 0; row < 3; row++)
       for (int col = 0; col < 3; col++)
-         if (grid[row+boxStartRow][col+boxStartCol] == num)
-            return true;
-   return false;
+         if (sudoku[row + startRow][col + startCol] == num)
+            return (1);
+   return (0);
 }
-void sudokuGrid(){ //print the sudoku grid after solve
-   for (int row = 0; row < N; row++){
-      for (int col = 0; col < N; col++){
-         if(col == 3 || col == 6)
-            cout << " | ";
-         cout << grid[row][col] <<" ";
+
+void printSudoku(void)
+{
+    cout << "<--------SOLUTION-------->" << endl;
+   for (int fila = 0; fila < 9; fila++)
+   {
+        for (int col = 0; col < 9; col++)
+        {
+            cout << sudoku[fila][col] << " ";
+            if (col == 2 || col == 5) {
+                cout << " | ";
+            }
+        }
+        cout << endl;
+        if (fila == 2 || fila == 5)
+        {
+            for (int i = 0; i < 9; i++)
+                cout << "---";
+            cout << endl;
+        }
+    }
+}
+
+int isEmpty(int &row, int &col)
+{ 
+   for (row = 0; row < 9; row++)
+      for (col = 0; col < 9; col++)
+         if (sudoku[row][col] == 0)
+            return (1);
+   return (0);
+}
+
+int isValidPlace(int row, int col, int num){
+    if (isInRow(row, num) || isInCol(col, num) || isInBox(row - row%3, col - col%3, num))
+        return(0);
+    return (1);
+}
+
+int solve(void){
+   int row;
+   int col;
+   
+   if (!isEmpty(row, col))
+      return (1);
+   for (int num = 1; num <= 9; num++)
+   {
+      if (isValidPlace(row, col, num))
+      { 
+         sudoku[row][col] = num;
+         if (solve())
+            return (1);
+         sudoku[row][col] = 0;
       }
-      if(row == 2 || row == 5){
-         cout << endl;
-         for(int i = 0; i<N; i++)
-            cout << "---";
-      }
-      cout << endl;
    }
+   return (0);
 }
-bool findEmptyPlace(int &row, int &col){ //get empty location and update row and column
-   for (row = 0; row < N; row++)
-      for (col = 0; col < N; col++)
-         if (grid[row][col] == 0) //marked with 0 is empty
-            return true;
-   return false;
-}
-bool isValidPlace(int row, int col, int num){
-   //when item not found in col, row and current 3x3 box
-   return !isPresentInRow(row, num) && !isPresentInCol(col, num) && !isPresentInBox(row - row%3 ,
-col - col%3, num);
-}
-bool solveSudoku(){
-   int row, col;
-   if (!findEmptyPlace(row, col))
-      return true; //when all places are filled
-   for (int num = 1; num <= 9; num++){ //valid numbers are 1 - 9
-      if (isValidPlace(row, col, num)){ //check validation, if yes, put the number in the grid
-         grid[row][col] = num;
-         if (solveSudoku()) //recursively go for other rooms in the grid
-            return true;
-         grid[row][col] = 0; //turn to unassigned space when conditions are not satisfied
-      }
-   }
-   return false;
-}
+
 int main(){
-   if (solveSudoku())
-      sudokuGrid();
+   if (solve())
+      printSudoku();
    else
-      std::cout << "No solution exists" << std::endl;
+      cout << "Cannot solve" << endl;
 }
