2b8eb40f7d6c912f79af28e9f1fa17ccadb308c2
Pablo Bengoechea Navarro <pbengoec@c3r7s3.42malaga.com>
2024-06-19 19:20:43 +0200

    update

---
+++ cursus/CPP/CPP 09/ex00/src/BitcoinExchange.cpp
@@ -0,0 +1,16 @@
+#include "BitcoinExchange.hpp"
+
+BitcoinExchange::BitcoinExchange()
+{}
+
+BitcoinExchange::BitcoinExchange(char *file)
+{}
+
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
+{}
+
+BitcoinExchange::~BitcoinExchange()
+{}
+
+BitcoinExchange BitcoinExchange::operator=(const BitcoinExchange &other)
+{}
--- cursus/CPP/CPP 09/ex00/src/main.cpp
+++ cursus/CPP/CPP 09/ex00/src/main.cpp
@@ -1,14 +1,13 @@
-#include "easyfind.hpp"
+#include "BitcoinExchange.hpp"
 #include <iostream>
 #include <vector>
 
-int main(void) {
-	int arr[] = {1,2,3,4,5,6,7,8,9};
-	size_t size = sizeof(arr) / sizeof(int);
-	std::vector<int> vect(arr, arr + size);
-
-	easyfind(vect, 4);
-	easyfind(vect, 10);
-
-	return (0);
+int main(int argc, char** argv) {
+	if(argc != 2)
+	{
+		std::cerr<<"Error: bad input"<<std::endl;
+		return(1);
+	}
+	BitcoinExchange exchange(argv[1]);
+	return(0);
 }
